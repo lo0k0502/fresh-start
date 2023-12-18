50ed0a7633241229e90988646fe89c0571147ae7
lo0k0502 <thomas85514@gmail.com>
2023-12-19 02:27:40 +0800

    add 404 redirection

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,3 +1,10 @@
+export const handler = () => {
+  const headers = new Headers();
+  headers.set('location', '/');
+
+  return new Response(null, { status: 302, headers });
+};
+
 export default function Error404() {
   return <div class='text-4xl font-bold'>404 - Page not found</div>;
 }
